9c6cf4eeb451e0faee1139d62524d11940e6bc68
yxq <yxq@disanbo.com>
2020-01-22 17:21:28 +0800

    solution of problem 37

--- solutions/35_search-insert-position/solution_test.go
+++ solutions/35_search-insert-position/solution_test.go
@@ -2,11 +2,19 @@ package _5_search_insert_position
 
 func searchInsert(nums []int, target int) int {
 	l := len(nums)
-	if l == 0 {
+	if l == 0 || target < nums[0] {
 		return 0
 	}
-	for left, right := 0, l; left < right; {
+	var left, right int
+	for left, right = 0, l; left < right; {
 		mid := (left + right) /2
-		if nums[mid] >
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid
+		} else {
+			return mid
+		}
 	}
+	return right
 }
